eba0f27ccba503a090541cfe78532c1b12a702a9
kaznak <kaznak.at.work@gmail.com>
2021-01-18 22:49:24 +0900

    # update test

--- test/pages/index.test.tsx
+++ test/pages/index.test.tsx
@@ -1,17 +1,13 @@
 import React from 'react'
-import { render, fireEvent } from '../testUtils'
-import { Home } from '../../pages/index'
+import { render } from '../testUtils'
+import { Index, getStaticProps } from '../../pages'
+
+const { props } = getStaticProps()
 
 describe('Home page', () => {
   it('matches snapshot', () => {
-    const { asFragment } = render(<Home />, {})
-    expect(asFragment()).toMatchSnapshot()
-  })
 
-  it('clicking button triggers alert', () => {
-    const { getByText } = render(<Home />, {})
-    window.alert = jest.fn()
-    fireEvent.click(getByText('Test Button'))
-    expect(window.alert).toHaveBeenCalledWith('With typescript and Jest')
+    const { asFragment } = render(<Index {...props} />, {})
+    expect(asFragment()).toMatchSnapshot()
   })
 })
