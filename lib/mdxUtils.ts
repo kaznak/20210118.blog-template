90d5dcd468cf18cdabe6eb7e2034e20f119b9f48
kaznak <kaznak.at.work@gmail.com>
2021-01-19 22:01:34 +0900

    lib/mdxUtils.ts: update

--- lib/mdxUtils.ts
+++ lib/mdxUtils.ts
@@ -3,41 +3,65 @@ import path from 'path'
 import matter from 'gray-matter'
 
 // POSTS_PATH is useful when you want to get the path to a specific file
-export const POSTS_PATH = path.join(process.cwd(), 'posts')
-
-// postFilePaths is the list of all mdx files inside the POSTS_PATH directory
-export const postFilePaths = fs
-  .readdirSync(POSTS_PATH)
-  // Only include md(x) files
-  .filter((path) => /\.mdx?$/.test(path))
+const POSTS_PATH = path.join(process.cwd(), 'posts')
 
 interface GetPostsOptions {
   dropOrig: boolean
+  postsPath: string
 }
 
 const getPostsDefaultOptions: GetPostsOptions = {
   dropOrig: true,
+  postsPath: POSTS_PATH,
 }
 
-export function getPosts(options: Partial<GetPostsOptions> = {}) {
-  const { dropOrig } = Object.assign(getPostsDefaultOptions, options)
-  return postFilePaths.map((filePath) => {
-    const source = fs.readFileSync(path.join(POSTS_PATH, filePath))
-    const gmData = matter(source)
-
-    const slug = filePath.replace(/\.mdx?$/, '')
-
-    if (dropOrig) {
-      // next.js raise error, if orig property is.
-      delete gmData.orig
-    }
-
-    return {
-      ...gmData,
-      filePath,
-      slug,
-    }
-  })
+function getPostFilePaths(options: Partial<GetPostsOptions> = {}) {
+  const { postsPath } = Object.assign(getPostsDefaultOptions, options)
+  return (
+    fs
+      .readdirSync(postsPath)
+      // Only include md(x) files
+      .filter((path) => /\.mdx?$/.test(path))
+  )
 }
 
-export const posts = getPosts()
+// postFilePaths is the list of all mdx files inside the POSTS_PATH directory
+const postFilePaths = getPostFilePaths()
+
+const baseNameToSlug = (baseName: string) => baseName.replace(/\.mdx?$/, '')
+
+function getPost(baseName: string, options: Partial<GetPostsOptions> = {}) {
+  const { dropOrig, postsPath } = Object.assign(getPostsDefaultOptions, options)
+
+  const source = fs.readFileSync(path.join(postsPath, baseName))
+  const gmData = matter(source)
+
+  if (dropOrig) {
+    // next.js raise error, if orig property is.
+    delete gmData.orig
+  }
+
+  const slug = baseNameToSlug(baseName)
+
+  return {
+    ...gmData,
+    slug,
+  }
+}
+
+export function getPostFromSlug(
+  slug: string,
+  options?: Partial<GetPostsOptions>
+) {
+  return getPost(`${slug}.mdx`, options)
+}
+
+function getPosts(options?: Partial<GetPostsOptions>) {
+  return getPostFilePaths(options).map((filePath) => getPost(filePath, options))
+}
+export const posts = postFilePaths.map(getPost)
+
+function getSlugs(options?: Partial<GetPostsOptions>) {
+  return getPostFilePaths(options).map(baseNameToSlug)
+}
+export const slugs = postFilePaths.map(baseNameToSlug)
