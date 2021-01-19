f5ba0ef29900621ce5f86ed7da3c75d9ba380028
kaznak <kaznak.at.work@gmail.com>
2021-01-19 14:35:32 +0900

    pages/_app.tsx: setup material ui

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,26 @@
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
+
+import '@fontsource/roboto/300.css'
+import '@fontsource/roboto/400.css'
+import '@fontsource/roboto/500.css'
+import '@fontsource/roboto/700.css'
+
+import { ThemeProvider, useTheme } from '@material-ui/core/styles'
+import CssBaseline from '@material-ui/core/CssBaseline'
+import { mUiTheme } from '../lib/materialUiUtils'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={mUiTheme}>
+      <Head>
+        {/* PWA primary color */}
+        <meta name="theme-color" content={useTheme().palette.primary.main} />
+      </Head>
+      <CssBaseline />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  )
 }
 
 export default MyApp
